b911ccb736e9e6cf6e924fe0d66f2eadc1480720
christian <christianzagazeta@icloud.com>
2019-10-28 14:59:30 -0400

    Added Dummy Data Dummy data, 'context', added to render

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,9 +1,29 @@
 from django.shortcuts import render
 
 
+# dummy data
+post = [
+    {
+        'author': 'Christian',
+        'title': 'Blog Post #1',
+        'content': 'First post content.',
+        'date_posted': 'October 28, 2019'
+    },
+    {
+        'author': 'Bethany',
+        'title': 'Blog Post #2',
+        'content': 'Second post content.',
+        'date_posted': 'October 22, 2019'
+    }
+]
+
+
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': post
+    }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return render(request, 'blog/about.html')
+    return render(request, 'blog/about.html', {'title': 'About'})
