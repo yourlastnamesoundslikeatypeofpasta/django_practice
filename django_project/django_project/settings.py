b61931deb4d127d8654730c7e521afef23c7f45a
christian <christianzagazeta@icloud.com>
2019-12-01 01:15:00 -0500

    [FIXED] app --> apps

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'users.app.UsersConfig',
+    'users.apps.UsersConfig',
     'blog.apps.BlogConfig',
     'django.contrib.admin',
     'django.contrib.auth',
