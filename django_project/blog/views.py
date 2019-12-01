06661c84aaf8c9edde02437de11eb8385a7e90f2
christian <christianzagazeta@icloud.com>
2019-12-01 00:37:41 -0500

    Data Removed dummy data. Added Post (ORM) object as data source.

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,26 +1,10 @@
 from django.shortcuts import render
-
-
-# dummy data
-post = [
-    {
-        'author': 'Christian',
-        'title': 'Blog Post #1',
-        'content': 'First post content.',
-        'date_posted': 'October 28, 2019'
-    },
-    {
-        'author': 'Bethany',
-        'title': 'Blog Post #2',
-        'content': 'Second post content.',
-        'date_posted': 'October 22, 2019'
-    }
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': post
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
