57689036ff333860c4186b840885e8ed8ff4b951
christian <christianzagazeta@icloud.com>
2019-12-01 01:15:21 -0500

    New Route Added the register/ route.

--- django_project/django_project/urls.py
+++ django_project/django_project/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
-
+from users import views as user_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('register/', user_views.register, name='register'),
     path('blog/', include('blog.urls')),
     path('', include('blog.urls')),
 ]
