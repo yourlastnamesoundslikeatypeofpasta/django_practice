cdd2575a6df6031892d335043b5a75cb110cfa3a
christian <christianzagazeta@icloud.com>
2019-12-02 15:20:15 -0500

    Updated Redirect, and Added View Register will now redirect to 'login'
    Added View:
    profile*
    *login required view

--- django_project/users/views.py
+++ django_project/users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm
 
 
@@ -11,7 +12,12 @@ def register(request):
             username = form.cleaned_data.get('username')
             first = form.cleaned_data.get('first_name')
             messages.success(request, f'Thank you for joining us {first}. Your account has been created: {username}')
-            return redirect('blog-home')
+            return redirect('login')
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
+
+
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')
