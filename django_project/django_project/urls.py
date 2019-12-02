23298d119cf2b362d1dededb3a7cb7c7202b4885
christian <christianzagazeta@icloud.com>
2019-12-02 15:17:38 -0500

    Added Routes Added the following routes - profile - login - logout

--- django_project/django_project/urls.py
+++ django_project/django_project/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth import views as auth_viws
 from django.urls import path
 from django.urls import include
 from users import views as user_views
@@ -21,6 +22,9 @@ from users import views as user_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', user_views.register, name='register'),
+    path('profile/', user_views.profile, name='profile'),
+    path('login/', auth_viws.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', auth_viws.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('blog/', include('blog.urls')),
     path('', include('blog.urls')),
 ]
