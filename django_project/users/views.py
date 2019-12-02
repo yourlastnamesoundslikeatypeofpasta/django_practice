5ae0bc3940fee0f213646df28e7e90792a993db4
christian <christianzagazeta@icloud.com>
2019-12-01 19:34:40 -0500

    Save Updated UserCreationForm --> UserRegisterForm Form data will now be saved in db. Added success message

--- django_project/users/views.py
+++ django_project/users/views.py
@@ -1,7 +1,17 @@
-from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import UserRegisterForm
 
 
 def register(request):
-    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            first = form.cleaned_data.get('first_name')
+            messages.success(request, f'Thank you for joining us {first}. Your account has been created: {username}')
+            return redirect('blog-home')
+    else:
+        form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
