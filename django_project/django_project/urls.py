9da21a7401f1d4def0259e45eb04bdb0b74ef9ed
christian <christianzagazeta@icloud.com>
2019-12-03 00:44:48 -0500

    Added Routes - login - logout - MEDIA_URL

--- django_project/django_project/urls.py
+++ django_project/django_project/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.contrib.auth import views as auth_viws
+from django.contrib.auth import views as auth_views
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import include
 from users import views as user_views
 
@@ -23,8 +25,11 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', user_views.register, name='register'),
     path('profile/', user_views.profile, name='profile'),
-    path('login/', auth_viws.LoginView.as_view(template_name='users/login.html'), name='login'),
-    path('logout/', auth_viws.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('blog/', include('blog.urls')),
     path('', include('blog.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
